5f587eac815621131c23825b5ed92b507bd8a862
budopest <abdo.mohamed5111995@gmail.com>
2020-09-27 02:53:03 +0200

    Create Dao and implementation for retrieving customers

---
+++ src/main/java/customer/management/system/dao/CustomerDaoInterface.java
@@ -0,0 +1,11 @@
+package customer.management.system.dao;
+
+import customer.management.system.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerDaoInterface {
+
+    public List<Customer> getCustomers();
+
+}
---
+++ src/main/java/customer/management/system/dao/HibernateCustomerDao.java
@@ -0,0 +1,27 @@
+package customer.management.system.dao;
+
+import customer.management.system.entity.Customer;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository(value = "HibernateCustomerDao")
+public class HibernateCustomerDao implements CustomerDaoInterface {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    @Transactional
+    public List<Customer> getCustomers() {
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        Query<Customer> query = currentSession.createQuery("from Customer",Customer.class);
+        return query.getResultList();
+    }
+}
