79af563a7c357117a04b629ac1b3d9257ccab714
budopest <abdo.mohamed5111995@gmail.com>
2020-10-15 20:24:59 +0200

    Add spring security for authentication using jdbc tables to configure users and roles

---
+++ src/main/java/customer/management/system/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,8 @@
+package customer.management.system.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer 
+						extends AbstractSecurityWebApplicationInitializer {
+
+}
